446c72b5c136aa38c1959b0a4de7e879175e7283
AntonPochernikov <36863354+AntonPochernikov@users.noreply.github.com>
2019-02-16 16:52:04 +0300

    ch1 ex13

---
+++ sicp/chapter1/13.rkt
@@ -0,0 +1,22 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define psi (/ (- 1 (sqrt 5)) 2))
+(define phi (/ (+ 1 (sqrt 5)) 2))
+
+(define (^ n exp)
+  (define (iter acc counter)
+    (if (= counter 0)
+         acc
+         (iter (* acc n) (- counter 1))))
+  (iter 1 exp))
+
+(define (fib n)
+   (/ (^ phi n) (sqrt 5)))
+
+(check-equal? (round (fib 3)) 2.0)
+(check-equal? (round (fib 5)) 5.0)
+(check-equal? (round (fib 7)) 13.0)
+
+(display "all suits passed!")
